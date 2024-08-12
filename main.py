3493c10a9f941aad643af8da67741ad4135e5111
hollali <dheztinykartel@gmail.com>
2024-08-12 13:32:40 +0000

    "Added phone and bill number fields, and a search button to customer details frame."

--- main.py
+++ main.py
@@ -11,9 +11,24 @@ customer_details_frame=LabelFrame(root,text="Customer Details",font=('times new 
 customer_details_frame.pack()
 
 nameLabel=Label(customer_details_frame,text='Name:',font=('times new roman',15,'bold'),bg='gray20',fg='white')
-nameLabel.grid(row=0,column=0,padx=20)
+nameLabel.grid(row=0,column=0,padx=20,pady=2)
 
 nameEntry=Entry(customer_details_frame,font=('arial',15),bd=7,width=18)
-nameEntry.grid(row=0,column=1)
+nameEntry.grid(row=0,column=1,padx=8)
 
-root.mainloop()
+phoneLabel=Label(customer_details_frame,text='Phone:',font=('times new roman',15,'bold'),bg='gray20',fg='white')
+phoneLabel.grid(row=0,column=2,padx=20,pady=2)
+
+phoneEntry=Entry(customer_details_frame,font=('arial',15),bd=7,width=18)
+phoneEntry.grid(row=0,column=3,padx=8)
+
+billnumberLabel=Label(customer_details_frame,text='Bill Number:',font=('times new roman',15,'bold'),bg='gray20',fg='white')
+billnumberLabel.grid(row=0,column=4,padx=20,pady=2)
+
+billnumberEntry=Entry(customer_details_frame,font=('arial',15),bd=7,width=18)
+billnumberEntry.grid(row=0,column=5,padx=8)
+
+searchButton=Button(customer_details_frame,text='Search')
+searchButton.grid(row=0,column=6,padx=8)
+
+root.mainloop() 
