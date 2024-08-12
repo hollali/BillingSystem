b1560fb53fce05fb47d90fe75e02db5e6ad65102
hollali <dheztinykartel@gmail.com>
2024-08-12 10:10:19 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import * 
+
+root=Tk()
+root.title("Reatail Billing System")
+root.geometry("1270x685")
+root.iconbitmap()
+headingLabel=Label(root,text="Retail Billing System",font=('times new roman',30))
+headingLabel.pack()
+root.mainloop()
