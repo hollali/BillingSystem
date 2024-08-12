35335312648d3fcc7d9c27dc91b45761bf69f18b
hollali <dheztinykartel@gmail.com>
2024-08-12 14:14:09 +0000

    "Updated customer details frame padding, label text, and button width in main.py"

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ headingLabel=Label(root,text="Retail Billing System",font=('times new roman',30,
 headingLabel.pack(fill=X)
 
 customer_details_frame=LabelFrame(root,text="Customer Details",font=('times new roman',15,'bold'),bg='gray20',fg='gold',bd=12,relief=GROOVE)
-customer_details_frame.pack(fill=X)
+customer_details_frame.pack(fill=X,pady=10)
 
 nameLabel=Label(customer_details_frame,text='Name:',font=('times new roman',15,'bold'),bg='gray20',fg='white')
 nameLabel.grid(row=0,column=0,padx=20)
@@ -16,7 +16,7 @@ nameLabel.grid(row=0,column=0,padx=20)
 nameEntry=Entry(customer_details_frame,font=('arial',15),bd=7,width=18)
 nameEntry.grid(row=0,column=1,padx=8)
 
-phoneLabel=Label(customer_details_frame,text='Phone:',font=('times new roman',15,'bold'),bg='gray20',fg='white')
+phoneLabel=Label(customer_details_frame,text='Phone Number:',font=('times new roman',15,'bold'),bg='gray20',fg='white')
 phoneLabel.grid(row=0,column=2,padx=20,pady=2)
 
 phoneEntry=Entry(customer_details_frame,font=('arial',15),bd=7,width=18)
@@ -28,7 +28,7 @@ billnumberLabel.grid(row=0,column=4,padx=20,pady=2)
 billnumberEntry=Entry(customer_details_frame,font=('arial',15),bd=7,width=18)
 billnumberEntry.grid(row=0,column=5,padx=8)
 
-searchButton=Button(customer_details_frame,text='SEARCH',font=('arial',12,'bold'),bd=7)
+searchButton=Button(customer_details_frame,text='SEARCH',font=('arial',12,'bold'),bd=7,width=10)
 searchButton.grid(row=0,column=6,padx=20,pady=8)
 
 root.mainloop() 
