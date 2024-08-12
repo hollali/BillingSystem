26000f650c7f1b3ff567cc41835c09d5225a995b
hollali <dheztinykartel@gmail.com>
2024-08-12 14:01:41 +0000

    "Updated GUI layout and styling for Retail Billing System"

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from tkinter import *
 root=Tk()
 root.title("Reatail Billing System")
 root.geometry("1270x685")
-root.iconbitmap()
+root.iconbitmap('')
 headingLabel=Label(root,text="Retail Billing System",font=('times new roman',30,'bold'),bg='gray20',fg='gold',bd=12,relief=GROOVE)
 headingLabel.pack(fill=X)
 
 customer_details_frame=LabelFrame(root,text="Customer Details",font=('times new roman',15,'bold'),bg='gray20',fg='gold',bd=12,relief=GROOVE)
-customer_details_frame.pack()
+customer_details_frame.pack(fill=X)
 
 nameLabel=Label(customer_details_frame,text='Name:',font=('times new roman',15,'bold'),bg='gray20',fg='white')
-nameLabel.grid(row=0,column=0,padx=20,pady=2)
+nameLabel.grid(row=0,column=0,padx=20)
 
 nameEntry=Entry(customer_details_frame,font=('arial',15),bd=7,width=18)
 nameEntry.grid(row=0,column=1,padx=8)
@@ -28,7 +28,7 @@ billnumberLabel.grid(row=0,column=4,padx=20,pady=2)
 billnumberEntry=Entry(customer_details_frame,font=('arial',15),bd=7,width=18)
 billnumberEntry.grid(row=0,column=5,padx=8)
 
-searchButton=Button(customer_details_frame,text='Search')
-searchButton.grid(row=0,column=6,padx=8)
+searchButton=Button(customer_details_frame,text='SEARCH',font=('arial',12,'bold'),bd=7)
+searchButton.grid(row=0,column=6,padx=20,pady=8)
 
 root.mainloop() 
